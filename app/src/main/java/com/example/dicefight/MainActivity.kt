584072ffe2f54d3fd9a49de0f2cf209d4d19621b
SrFail <ivanbermejomelia@gmail.com>
2025-10-22 12:28:24 +0200

    Primer layout

---
+++ app/src/main/java/com/example/dicefight/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.dicefight
+
+import android.os.Bundle
+import android.text.Layout
+import android.text.style.BackgroundColorSpan
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.dicefight.ui.theme.DiceFightTheme
+import com.example.dicefight.ui.theme.MoradoOscuro
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceFightApp()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceFightApp() {
+    VistaApp()
+}
+
+
+@Composable
+fun VistaApp(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.background(Color.Blue)
+            .fillMaxSize()
+    ){
+        Box (
+            modifier = Modifier
+                .weight(2f)
+                .fillMaxWidth()
+                .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+                .background(MoradoOscuro)
+
+        ){
+            Image(
+                painter = painterResource(R.drawable.monster1),
+                contentDescription = "Dado",
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(10.dp)
+                .background(Color.Black)
+        ){
+            Image(
+                painter = painterResource(CargarDados().get(1)),
+                contentDescription = "Dado",
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
+
+fun CargarDados(): List<Int> = listOf(
+    R.drawable.dice_1,
+    R.drawable.dice_2,
+    R.drawable.dice_3,
+    R.drawable.dice_4,
+    R.drawable.dice_5,
+    R.drawable.dice_6
+)
